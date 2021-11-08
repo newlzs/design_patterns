74199aa84013e5a439d19375663a35d797064963
newlzs <1581004928@qq.com>
2021-11-08 21:09:17 +0800

    设计模式

---
+++ src/main/java/facade/SubSystem2.java
@@ -0,0 +1,12 @@
+package facade;
+
+/**
+ * @author Lzs
+ * @date 2021/11/5 21:58
+ * @description
+ */
+public class SubSystem2 {
+    public void m() {
+        System.out.println("Sub2");
+    }
+}
